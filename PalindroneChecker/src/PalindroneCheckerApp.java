b9bb1119d71a5e44b3d28ddd7a02161057f787d4
Avinash-1464 <ponneboinaavinash@gmail.com>
2026-02-25 10:30:00 +0530

    03 UC2-Print a Hardcoded Palindrome Result

--- PalindroneChecker/src/PalindroneCheckerApp.java
+++ PalindroneChecker/src/PalindroneCheckerApp.java
@@ -1,7 +1,19 @@
 public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to palindromchecker app");
-
+        String str = "madam";
+        boolean ispal = true;
+        for( int i = 0;i<str.length()/2;i++){
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                ispal = false;
+                break;
+            }
+        }
+        if(ispal){
+            System.out.println(str+" is palindrome");
+        }
+        else{
+            System.out.println(str+" is not a palindrome");
+        }
     }
 }
