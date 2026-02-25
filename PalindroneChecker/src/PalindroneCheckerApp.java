226502d90361c6bfbe1dcf40bc6e6da424a1b7ca
Avinash-1464 <ponneboinaavinash@gmail.com>
2026-02-25 09:36:09 +0530

    UC1: Application Entry & Welcome Message

--- PalindroneChecker/src/PalindroneCheckerApp.java
+++ PalindroneChecker/src/PalindroneCheckerApp.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
 public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to palindromchecker app");
 
     }
 }
